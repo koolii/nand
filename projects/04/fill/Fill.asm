cd433005f579edd1e32b3f384c4f61c955b0dd40
koolii <kuriyama@createentrance.com>
2018-01-19 14:06:15 +0100

    :+1: add [WIP] Fill.asm doesn't implement that it makes all pixel black or white

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -1,14 +1,31 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Fill.asm
-
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+(BLACK)
+@SCREEN
+A=1
+@LOOP
+0;JMP
+
+(WHITE)
+@SCREEN
+A=1
+@LOOP
+0;JMP
+
+(LOOP)
+@KBD
+D=A
+@BLACK
+D;JNE // ゼロだったら黒くする
+
+@WHITE
+D;JEQ // ゼロ以外だったら黒くする
+
+@LOOP
+0;JMP
