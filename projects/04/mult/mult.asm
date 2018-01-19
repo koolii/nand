b377099a1d2c7b15cbdce229e91cdb62a476fa12
koolii <kuriyama@createentrance.com>
2018-01-19 07:28:28 +0100

    :+1: [WIP] add making 100 times loop as step1 into Mult.asm

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,26 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+@i
+M=0
+
+(LOOP)
+@i
+D=M // Dにiの値を一時的に確保
+
+@100
+D=D-A // Aに100を代入 && i = i - 100
+
+@END
+D;JGT // 100回ループさせる
+
+@i
+M=M+1 // インクリメント
+
+@LOOP
+0;JMP // LOOPを続ける
+
+(END)
+@END
+0;JMP // fin
